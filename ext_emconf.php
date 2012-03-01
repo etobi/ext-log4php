46a4abd1a82e2e95cd369c16246bbca24a46c1fc
Tobias Liebig <tobias.liebig@typo3.org>
2012-03-01 19:06:52 +0100

    include log4php lib

---
+++ ext_emconf.php
@@ -0,0 +1,33 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Log4Php',
+	'description' => 'Apache Log4Php',
+	'category' => 'misc',
+	'author' => 'Tobias Liebig',
+	'author_email' => 'work@etobi.de',
+	'author_company' => '',
+	'shy' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => '1',
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'version' => '0.0.1',
+	'constraints' => array(
+		'depends' => array(
+			'cms' => '',
+			'extbase' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+);
+
+?>
---
+++ ext_localconf.php
@@ -0,0 +1,8 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die('Access denied.');
+}
+
+require_once(t3lib_div::getFileAbsFileName('EXT:log4php/Resources/Private/Php/apache-log4php-2.2.1/src/main/php/Logger.php'));
+
+?>
