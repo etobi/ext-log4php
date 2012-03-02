09ca41156795b6a9d3cad0893990312e0c9ca9f8
Tobias Liebig <tobias.liebig@typo3.org>
2012-03-02 10:40:38 +0100

    [FEATURE] configurable config file

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,28 +5,12 @@ if (!defined('TYPO3_MODE')) {
 
 require_once(t3lib_div::getFileAbsFileName('EXT:log4php/Resources/Private/Php/apache-log4php-2.2.1/src/main/php/Logger.php'));
 
-Logger::configure(array(
-		'appenders' => array(
-			'default' => array(
-				'class' => 'LoggerAppenderFile',
-				'layout' => array(
-					'class' => 'LoggerLayoutPattern',
-					'params' => array(
-						'conversionPattern' => '%d{Y-m-d H:i:s.u} %c %-5p %m%n',
-					)
-				),
-				'params' => array(
-					'file' => PATH_site . 'typo3temp/log4php.log',
-					'append' => false
-				),
-			),
-		),
-		'rootLogger' => array(
-			'appenders' => array('default'),
-		),
-	));
+$extConf = unserialize($TYPO3_CONF_VARS['EXT']['extConf'][$_EXTKEY]);
+Logger::configure($extConf['configFile']);
 
+/*
 $logger = Logger::getLogger("default");
 $logger->info('Foobar');
+*/
 
 ?>
